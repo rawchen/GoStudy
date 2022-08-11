d300b612bbc70d68bd5b9b3854c5a3fc8c22c208
RawChen <2221999792@qq.com>
2022-08-11 23:59:22 +0800

    go play

--- main/main.go
+++ main/main.go
@@ -2,6 +2,20 @@ package main
 
 import "fmt"
 
+func testFunction(num1 int, num2 int) int {
+	return (num1 + num2)
+}
+
+func testPtr(num *int) {
+	*num = 20
+}
+
 func main() {
 	fmt.Println(123)
+	var a = 10
+	fmt.Printf("输出：%v", a)
+	fmt.Printf("输出：%v", &a)
+	//fmt.Printf("test: %v", testPtr())
+
+	fmt.Printf(string(testFunction(1, 2)))
 }
