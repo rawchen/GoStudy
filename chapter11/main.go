9ecd7c1da16c4ebad8da0f12cf6a68107e3e3cda
RawChen <2221999792@qq.com>
2022-09-27 23:57:56 +0800

    函数传参为指针

--- chapter11/main.go
+++ chapter11/main.go
@@ -1,9 +1,21 @@
-package chapter11
+package main
 
 import (
 	"GoStudy/utils"
+	"fmt"
 )
 
 func main() {
 	utils.PrintOk()
+
+	num := 20
+	fmt.Printf("num的地址=%v \n", &num)
+	test01(&num)
+}
+
+func test01(n1 *int) {
+	fmt.Printf("n1的地址=%v \n", &n1)
+	*n1 = *n1 + 10
+	fmt.Println("n1 = ", *n1)
+
 }
