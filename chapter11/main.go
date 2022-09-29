1cbac394d4132e1e9bfcafde4b9c39d7a68e5978
RawChen <2221999792@qq.com>
2022-09-29 23:55:25 +0800

    函数可变参数

--- chapter11/main.go
+++ chapter11/main.go
@@ -11,6 +11,9 @@ func main() {
 	num := 20
 	fmt.Printf("num的地址=%v \n", &num)
 	test01(&num)
+
+	result := sum(1, 2, 3, 5)
+	fmt.Println(result)
 }
 
 func test01(n1 *int) {
@@ -18,4 +21,13 @@ func test01(n1 *int) {
 	*n1 = *n1 + 10
 	fmt.Println("n1 = ", *n1)
 
+}
+
+func sum(n1 int, args ...int) int {
+	sum := n1
+	// 遍历args
+	for i := range args {
+		sum += args[i]
+	}
+	return sum
 }
