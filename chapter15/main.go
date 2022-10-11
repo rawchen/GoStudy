8464dbb7eb8f562a0987db4b9211db0bdad21469
RawChen <2221999792@qq.com>
2022-10-11 22:43:33 +0800

    go字符串函数

--- chapter15/main.go
+++ chapter15/main.go
@@ -55,4 +55,40 @@ func main() {
 	// 不区分大小写的字符串比较
 	result10 := strings.EqualFold("abc", "Abc")
 	fmt.Println("result10 =", result10)
+
+	// 子串第一次出现的位置，没有返回-1
+	int11 := strings.Index("abcdef", "bcd")
+	fmt.Println("int11 =", int11)
+
+	// 子串最后一次出现的位置，没有返回-1
+	int12 := strings.LastIndex("abcdefabc", "abc")
+	fmt.Println("int12 =", int12) // 6
+
+	// 子串替换，n则为替换几个，-1全部替换
+	str13 := strings.Replace("abcdefabc", "abc", "A", -1)
+	fmt.Println("str13 =", str13) // AdefA
+
+	// 按某字符分割
+	arr14 := strings.Split("123,456,789", ",")
+	fmt.Println("arr14 =", arr14)
+
+	// 大小写转换
+	str15 := strings.ToLower("Abc") // ToUpper()
+	fmt.Println("str15 =", str15)
+
+	// 去两边空格
+	str16 := strings.TrimSpace(" abc ")
+	fmt.Println("str16 =", str16)
+
+	// 去两边指定字符
+	str17 := strings.Trim("1abc21", "1") // TrimLeft() TrimRight()
+	fmt.Println("str17 =", str17)
+
+	// 以指定字符串开头
+	result20 := strings.HasPrefix("123456", "123")
+	fmt.Println("result20 =", result20)
+
+	// 以指定字符串结尾
+	result21 := strings.HasSuffix("123456", "456")
+	fmt.Println("result21 =", result21)
 }
