86b82612cd2f968edf14adb2220893470923371d
RawChen <2221999792@qq.com>
2022-10-16 23:59:06 +0800

    数组

--- chapter19/main.go
+++ chapter19/main.go
@@ -25,4 +25,25 @@ func main() {
 	// hens[2]地址为=0xc00000a430
 	fmt.Printf("hens的地址为=%p, hens[0]地址为=%p, hens[1]地址为=%p, hens[2]地址为=%p",
 		&hens, &hens[0], &hens[1], &hens[2])
+
+	// 4种初始化方式
+	var arr01 [3]int = [3]int{1, 2, 3}
+	fmt.Println("arr01 =", arr01)
+
+	var arr02 = [3]int{4, 5, 6}
+	fmt.Println("arr02 =", arr02)
+
+	// 可变容量
+	var arr03 = [...]int{7, 8, 9}
+	fmt.Println("arr03 =", arr03)
+
+	// 顺序自定义
+	var arr04 = [...]int{1: 50, 0: 100, 2: 200}
+	fmt.Println("arr04 =", arr04)
+
+	// for-range遍历数组
+	names := [...]string{"张三", "李四", "王五"}
+	for i, name := range names {
+		fmt.Println("索引为 =", i, "姓名为 =", name)
+	}
 }
