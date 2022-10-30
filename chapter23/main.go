9fce798a7648026c124ec7a2ef1a0b70726fd074
RawChen <2221999792@qq.com>
2022-10-30 23:59:21 +0800

    map crud

--- chapter23/main.go
+++ chapter23/main.go
@@ -15,4 +15,18 @@ func main() {
 	var c = map[string]string{"key": "123"}
 	fmt.Println(c)
 
+	// crud
+	c["key02"] = "456"
+	c["key"] = "111"
+	delete(c, "key")
+	fmt.Println(c)
+
+	val, ok := c["key02"]
+	if ok {
+		fmt.Println("c[\"no1\"]存在，值为=", val)
+	}
+
+	c = make(map[string]string)
+	fmt.Println(c)
+
 }
