497acc913fdffbbc7178067495a2e0a12c527aac
RawChen <2221999792@qq.com>
2022-11-23 00:10:25 +0800

    map值为结构体

--- chapter25/main.go
+++ chapter25/main.go
@@ -7,6 +7,12 @@ func modify(map1 map[int]int) {
 	fmt.Println("函数内：", map1)
 }
 
+type Student struct {
+	Name    string
+	Age     int
+	Address string
+}
+
 func main() {
 	// map为引用类型
 	map1 := make(map[int]int)
@@ -17,5 +23,27 @@ func main() {
 	modify(map1)
 
 	fmt.Println("主函数：", map1)
+
+	// 结构体
+	students := make(map[string]Student)
+	//student1 := Student{
+	//	Name:    "张三",
+	//	Age:     22,
+	//	Address: "深圳",
+	//}
+	student1 := Student{"张三", 22, "深圳"}
+	student2 := Student{"李四", 25, "厦门"}
+	students["s1"] = student1
+	students["s2"] = student2
+	fmt.Println(students)
+
+	// 遍历学生map
+	for k, v := range students {
+		fmt.Printf("学生编号是：%v \n", k)
+		fmt.Printf("结构体内容：%v \n", v)
+		fmt.Printf("学生姓名是：%v \n", v.Name)
+		fmt.Printf("学生年龄是：%v \n", v.Age)
+		fmt.Printf("学生地址是：%v \n", v.Address)
+	}
 
 }
