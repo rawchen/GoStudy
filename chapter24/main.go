73e669ad4eaf51b0c6de9f72436ed1365c676d57
RawChen <2221999792@qq.com>
2022-11-22 23:53:00 +0800

    map排序

--- chapter24/main.go
+++ chapter24/main.go
@@ -1,6 +1,9 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"sort"
+)
 
 func main() {
 	var monsters []map[string]string
@@ -32,10 +35,21 @@ func main() {
 	// 排序
 	map1 := make(map[int]int)
 	map1[10] = 100
-	map1[1] = 13
-	map1[4] = 56
+	map1[4] = 13
+	map1[1] = 56
 	map1[8] = 90
 
 	fmt.Println(map1)
+
+	var keys []int
+	for k, _ := range map1 {
+		keys = append(keys, k)
+	}
+	sort.Ints(keys)
+	fmt.Println(keys)
+
+	for _, k := range keys {
+		fmt.Printf("map1[%v]=%v \n", k, map1[k])
+	}
 
 }
