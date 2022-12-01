dce11d7ad95dd974097607a980abefa0fbeb5ee8
RawChen <2221999792@qq.com>
2022-12-01 23:47:01 +0800

    结构体内存分配

--- chapter27/main.go
+++ chapter27/main.go
@@ -26,4 +26,18 @@ func main() {
 	fmt.Println(p4)  // &{tom 0}
 	fmt.Println(&p4) // 0xc0000ca020
 	fmt.Println(*p4) // {tom 0}
+
+	// 结构体内存分配
+	var p5 Person
+	p5.Name = "李四"
+	p5.Age = 70
+
+	var p6 *Person = &p5
+
+	fmt.Println(p5.Name)
+	fmt.Println(p6.Name)
+
+	fmt.Printf("p5的地址为%p\n", &p5)             // 0xc0000080f0
+	fmt.Printf("p6的地址为%p p6的值为%p\n", &p6, p6) // 0xc00000a038  0xc0000080f0
+
 }
