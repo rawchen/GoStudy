7fb4633179178d8b50480f608a7c8a6675820b94
RawChen <2221999792@qq.com>
2023-02-12 23:40:35 +0800

    类型断言带检查

--- chapter40/main.go
+++ chapter40/main.go
@@ -19,6 +19,27 @@ func main() {
 	// b = a
 
 	b = a.(Point)
-
 	fmt.Println(b)
+
+	// 类型断言其它案例
+	//var x interface{}
+	//var b2 float32 = 1.1
+	//x = b2 // 空接口可接任意类型
+	//y := x.(float32)
+	//fmt.Printf("y 的类型是 %T 值是=%v", y, y)
+
+	// 类型断言(带检测)
+	var x interface{}
+	var b2 float32 = 1.1
+	x = b2 // 空接口可接任意类型
+	y, ok := x.(float64)
+
+	if ok == true {
+		fmt.Println("covert success")
+		fmt.Printf("y 的类型是 %T 值是=%v", y, y)
+	} else {
+		fmt.Println("covert fail")
+	}
+
+	fmt.Println("go on...")
 }
