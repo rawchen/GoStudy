b73219b11ed1f9aef8cbb3bded94be4deede7f64
RawChen <2221999792@qq.com>
2023-04-10 23:43:19 +0800

    互斥锁sync.Mutex

--- chapter50/main.go
+++ chapter50/main.go
@@ -3,24 +3,30 @@ package main
 import (
 	"fmt"
 	"runtime"
+	"sync"
 )
 
 var (
 	myMap = make(map[int]int, 10)
+	lock  sync.Mutex // 互斥锁
 )
 
 // 计算n!
 func test(n int) {
+	lock.Lock()
 	res := 1
 	for i := 1; i <= n; i++ {
 		res *= i
 	}
 
 	myMap[n] = res
+	lock.Unlock()
 }
 
 // 多线程同时占用myMap资源
 // fatal error: concurrent map writes
+// go build -race main.go
+// 或者加入互斥锁
 func main() {
 	cpuNum := runtime.NumCPU()
 	fmt.Printf("cpu数量=%v\n", runtime.NumCPU()) // 12
